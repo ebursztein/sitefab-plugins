adeb7b5f702ffd0aebb4f60a8e22bc8403396eaa
Elie <git@elie.net>
2019-12-30 14:35:22 -0800

    Image info refactored and working

--- site/rendering/sitemap/sitemap.py
+++ site/rendering/sitemap/sitemap.py
@@ -7,11 +7,11 @@ class Sitemap(SiteRendering):
     def process(self, unused, site, config):
         template_name = config.template
 
-        # Loading
-        try:
-            template = site.jinja2.get_template(template_name)
-        except:
-            return SiteFab.ERROR, "sitemap", "can't find template:%s" % template_name
+        # Loading template
+        if template_name not in site.jinja2.list_templates():
+            return SiteFab.ERROR, "sitemap", ("template %s not found" %
+                                              template_name)
+        template = site.jinja2.get_template(template_name)
 
         # Rendering
         post_list = []
@@ -29,24 +29,27 @@ class Sitemap(SiteRendering):
             if post.meta.microdata_type == "WebSite":  # main page
                 post.meta.priority = 1.0
                 post.meta.frequency = "daily"
-            if post.meta.microdata_type == "CollectionPage":  # publications page
+            if post.meta.microdata_type == "CollectionPage":  # tags
                 post.meta.priority = 0.8
                 post.meta.frequency = "daily"
 
             if post.meta.banner:
-                post.meta.sitemap_banner_url = "%s%s" % (site.config.url, post.meta.banner)
+                post.meta.sitemap_banner_url = "%s%s" % (site.config.url,
+                                                         post.meta.banner)
             else:
-                post.meta.sitemap_banner_url = "%s/static/images/banner/default.png" % site.config.url
+                post.meta.sitemap_banner_url = "%s/static/images/banner/default.png" % site.config.url  # noqa
             post_list.append(post)
 
         for collection in site.posts_by_category.get_as_list():
             # add priority and frequency
             collection.meta.priority = 0.7
             collection.meta.frequency = "daily"
-            collection.meta.sitemap_banner_url = "%s/static/images/banner/default.png" % site.config.url
+            collection.meta.sitemap_banner_url = "%s/static/images/banner/default.png" % site.config.url  # noqa
 
+        collections = site.posts_by_category.get_as_list()
         try:
-            rv = template.render(posts=post_list, collections=site.posts_by_category.get_as_list())
+            rv = template.render(posts=post_list,
+                                 collections=collections)
         except Exception as e:
             return (SiteFab.ERROR, "sitemap", e)
 
@@ -54,5 +57,6 @@ class Sitemap(SiteRendering):
         path = site.get_output_dir()
         files.write_file(path, 'sitemap.xml', rv)
 
-        log_info = "template used:%s<br>ouput:%ssitemap.xml" % (template_name, path)
+        log_info = "template used:%s<br>ouput:%ssitemap.xml" % (template_name,
+                                                                path)
         return SiteFab.OK, "sitemap", log_info
