e4904b0937f23dc452ca8b3d1bb9c733a4e0c5d7
Elie <git@elie.net>
2019-12-29 19:30:05 -0800

    copy dir refactoring

--- site/preparsing/copy_dir/copy_dir.py
+++ site/preparsing/copy_dir/copy_dir.py
@@ -1,7 +1,19 @@
 import shutil
 from tqdm import tqdm
+from pathlib import Path
+
 from sitefab.plugins import SitePreparsing
 from sitefab.SiteFab import SiteFab
+
+PROGBAR = None
+
+
+def _logprogress(path, names):
+    """"log shutil progress
+    see here: https://docs.python.org/2/library/shutil.html#shutil.Error
+    """
+    PROGBAR.update()
+    return []   # nothing will be ignore
 
 
 class CopyDir(SitePreparsing):
@@ -13,27 +25,39 @@ class CopyDir(SitePreparsing):
         """ Process the content of the site once
         :param FabSite site: the site object
         """
+        global PROGBAR
         log = ""
         errors = False
         targets = config.targets
-        progress_bar = tqdm(total=len(targets), unit=' dir', desc="Copying directories", leave=False)
+
         for target in targets:
-            try:
-                src, dst = target.split('>')
-                src = src.strip()
-                dst = dst.strip()
-            except:
+            if '>' not in target:
                 errors = True
-                log += "[Failed] target '%s' is not properly formated<br/>" % target
+                log += ("[Error] target '%s' is not properly formated<br/>" %
+                        target)
                 continue
+
+            src, dst = target.split('>')
+            src = src.strip()
+            dst = dst.strip()
+
+            PROGBAR = tqdm(desc='%s -> %s' % (src, dst), unit='files',
+                           leave=False)
+
+            src = site.config.root_dir / src
+            dst = site.config.root_dir / dst
+
             try:
-                shutil.copytree(src, dst)
-            except:
-                errors = True
-                log += "[Failed] failed to copy '%s' to '%s' <br/>" % (src, dst)
+                shutil.copytree(src, dst, ignore=_logprogress)
+            except:  # noqa
+                errors += 1
+                log += "[Failed] failed to copy '%s' to '%s' <br/>" % (src,
+                                                                       dst)
+                PROGBAR.close()
                 continue
-            progress_bar.update(1)
+
             log += "[OK]copied: '%s' to '%s'<br>" % (src, dst)
+            PROGBAR.close()
 
         if errors:
             return (SiteFab.ERROR, "CopyDir", log)
