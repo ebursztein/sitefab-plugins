75e81cb0196a462772f7469c479bc3c6e0be1255
Celine Bursztein <contact@celine.im>
2021-05-16 10:48:01 -0700

    fix for full_url on home

--- post/processor/compute_full_post_url/compute_full_post_url.py
+++ post/processor/compute_full_post_url/compute_full_post_url.py
@@ -7,11 +7,14 @@ class FullUrl(PostProcessor):
         if post.meta.permanent_url:
             permanent_url = post.meta.permanent_url.strip()
 
-            if site.config.url[-1] != "/" and permanent_url[0] != "/":
+            if permanent_url == "/":
+                post.meta.full_url = site.config.url
+                return SiteFab.OK, post.meta.title, post.meta.full_url
+
+            elif site.config.url[-1] != "/" and permanent_url[0] != "/":
                 post.meta.full_url = "%s/%s" % (site.config.url, permanent_url)
             elif site.config.url[-1] == "/" and permanent_url[0] == "/":
-                post.meta.full_url = "%s%s" % (site.config.url[:1],
-                                               permanent_url)
+                post.meta.full_url = "%s%s" % (site.config.url[:1], permanent_url)
             else:
                 post.meta.full_url = "%s%s" % (site.config.url, permanent_url)
 
